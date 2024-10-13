86192e6570ab1785d088b61ea842d6db7d2d2fac
hamzaazman <hamzaazman@outlook.com>
2024-10-13 06:40:52 +0300

    feat: add bottom navigation bar with Password and History items

--- app/src/main/java/com/hamzaazman/passwordgenerator/ui/navigation/NavigationGraph.kt
+++ app/src/main/java/com/hamzaazman/passwordgenerator/ui/navigation/NavigationGraph.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.hamzaazman.passwordgenerator.ui.history.HistoryScreen
 import com.hamzaazman.passwordgenerator.ui.password.PasswordScreen
 
 @Composable
@@ -20,6 +21,9 @@ fun NavigationGraph(
     ) {
         composable("Password") {
             PasswordScreen()
+        }
+        composable("History") {
+            HistoryScreen()
         }
     }
 }
